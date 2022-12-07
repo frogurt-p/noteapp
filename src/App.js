8abfdd9e18de97dbe8281ad15adb5ffb605bf8c2
frogurt-p <71208704+frogurt-p@users.noreply.github.com>
2022-12-07 09:41:52 +0700

    .env and firebase connection

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 import Navbar from './compo/Navbar/Navbar';
 import Card from './compo/Card/Card';
-
+import { db } from './firebase-connection';
+import {collection, getDocs} from 'firebase/firestore';
+import React from 'react';
 
 function App() {
+  const [data, setData] = React.useState({});
+  const fireData = collection(db, "user")
+  React.useEffect(()=> {
+    const fetchData = async () => {
+      const returnDocs = await getDocs(fireData)
+      console.log(returnDocs)
+    }
+
+    fetchData();
+  },[])
+
   return (
     <div className={styles.App}>
       <Navbar/>
