babc62622b81a2a9bd85f128c59943b141f9363b
frogurt-p <71208704+frogurt-p@users.noreply.github.com>
2022-12-12 09:36:58 +0700

    input assignment

--- src/compo/Card/AssignmentAdd/AssignmentAdd.js
+++ src/compo/Card/AssignmentAdd/AssignmentAdd.js
@@ -3,11 +3,17 @@ import styles from "./AssignmentAdd.module.css";
 import addIcon from "../../../resource/Union.svg"
 
 const AssignmentAdd = (props) => {
+  const [editState, setEditState] = React.useState(false);
+  const handleOnClick = () => {
+    setEditState((prevState) => !editState)
+  }
+
+
   return (
     <div className={styles.label}>
         {/* <div className={styles.avatar}>+</div> */}
-        <img src={addIcon} alt='log' className = {styles.avatar}/>
-        <p>Add</p>    
+        <img src={addIcon} alt='log' className = {editState ? styles.avatarRotate : styles.avatar} onClick={handleOnClick}/>
+        { editState ? <input className={styles.input}></input> : <p>Add</p>}    
     </div>
   )
 }
