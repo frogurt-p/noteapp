17c75b01819df671dfb7132149bad72319ddc728
frogurt-p <71208704+frogurt-p@users.noreply.github.com>
2023-01-04 09:13:06 +0700

    final

--- src/compo/Navbar/Navbar.js
+++ src/compo/Navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = (props) => {
   return (
     <div className={styles.navbar}>
         <h2 className={styles.title}>{`Notes app as : `}</h2>
-        <h2 className={styles.userName}>Jambrakitis</h2>
+        {props.user && <h2 className={styles.userName}>{props.user.displayName}</h2>}
 
         { props.user ? <div className={styles.signOut} onClick={() => auth.signOut()}>Sign Out</div> : null}
     </div>
